e7aede73acb17f167d435b579083f694bc3b16bd
phongdo120 <126934783+phongdo120@users.noreply.github.com>
2024-10-24 16:11:26 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChar = '*';
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn(object sender, EventArgs e)
+        {
+
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "admin" && password == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại", "Thông báo");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void hv(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
